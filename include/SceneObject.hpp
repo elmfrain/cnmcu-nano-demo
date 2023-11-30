edb8caf7a4dddc2dbb38ed42fea337e5ea9a57ce
elmfrain <elmfer10@gmail.com>
2023-11-30 00:58:18 -0600

    added scene objects

---
+++ include/SceneObject.hpp
@@ -0,0 +1,83 @@
+#pragma once
+
+#include <string>
+#include <forward_list>
+#include <unordered_map>
+#include <glm/glm.hpp>
+#include <glm/gtc/quaternion.hpp>
+
+#include "Logger.hpp"
+
+namespace em
+{
+    struct Transform
+    {
+        enum RotationMode
+        {
+            EULER_XYZ,
+            EULER_XZY,
+            EULER_YXZ,
+            EULER_YZX,
+            EULER_ZXY,
+            EULER_ZYX,
+            QUATERNION
+        };
+
+        glm::vec3 position;
+        glm::vec3 rotationEuler;
+        glm::quat rotationQuaternion;
+        glm::vec3 scale;
+
+        glm::vec3 offset;
+        RotationMode rotationMode;
+
+        Transform(const glm::vec3& position = glm::vec3(0.0f), const glm::vec3& scale = glm::vec3(1.0f));
+
+        glm::mat4 getMatrix() const;
+        glm::mat4 getInverseMatrix() const;
+        glm::quat getRotation() const;
+    };
+
+    class SceneObject
+    {
+    public:
+        enum Type
+        {
+            EMPTY,
+            MESH,
+            LIGHT,
+            CAMERA,
+            ROOT
+        };
+
+        SceneObject(Type type, const std::string& name = "Object");
+        virtual ~SceneObject();
+
+        SceneObject(const SceneObject& other) = delete;
+        SceneObject& operator=(const SceneObject& other) = delete;
+
+        virtual void update(float dt) = 0;
+        virtual void draw() = 0;
+
+        Type getType() const;
+        const std::string& getName() const;
+        Transform& getTransform();
+        void setName(const std::string& name);
+
+        void removeAllChildren();
+        void removeChild(SceneObject& child);
+        void addChild(SceneObject& child);
+    private:
+        Type m_type;
+        std::string m_name;
+
+        Transform m_transform;
+
+        SceneObject* m_parent;
+        std::forward_list<SceneObject*> m_children;
+    protected:
+        static std::unordered_map<std::string, SceneObject*>& getObjects();
+        static const char* getRootName();
+        static Logger logger;
+    };
+}
---
+++ src/SceneObject.cpp
@@ -0,0 +1,207 @@
+#include <SceneObject.hpp>
+
+using namespace em;
+
+Logger SceneObject::logger("SceneObject");
+
+Transform::Transform(const glm::vec3& position, const glm::vec3& scale)
+    : position(position)
+    , rotationEuler(0.0f)
+    , rotationQuaternion(1.0f, 0.0f, 0.0f, 0.0f)
+    , scale(scale)
+    , offset(0.0f)
+    , rotationMode(EULER_XYZ)
+{
+}
+
+glm::mat4 Transform::getMatrix() const
+{
+    const glm::vec3 flipZVec = glm::vec3(1.0f, 1.0f, -1.0f);
+
+    glm::mat4 matrix = glm::translate(glm::mat4(1.0f), position * flipZVec);
+    matrix *= glm::mat4_cast(rotationQuaternion);
+    matrix = glm::translate(matrix, offset * flipZVec);
+    matrix = glm::scale(matrix, scale);
+    return matrix;
+}
+
+glm::mat4 Transform::getInverseMatrix() const
+{
+    const glm::vec3 flipZVec = glm::vec3(1.0f, 1.0f, -1.0f);
+
+    glm::mat4 matrix = glm::scale(glm::mat4(1.0f), 1.0f / scale);
+    matrix = glm::translate(matrix, -offset * flipZVec);
+    matrix = glm::inverse(glm::mat4_cast(rotationQuaternion)) * matrix;
+    matrix = glm::translate(matrix, -position * flipZVec);
+    return matrix;
+}
+
+glm::quat Transform::getRotation() const
+{
+    switch(rotationMode)
+    {
+    case EULER_XYZ:
+        return glm::quat(rotationEuler);
+    case EULER_XZY:
+        return glm::quat(glm::vec3(rotationEuler.x, rotationEuler.z, rotationEuler.y));
+    case EULER_YXZ:
+        return glm::quat(glm::vec3(rotationEuler.y, rotationEuler.x, rotationEuler.z));
+    case EULER_YZX:
+        return glm::quat(glm::vec3(rotationEuler.y, rotationEuler.z, rotationEuler.x));
+    case EULER_ZXY:
+        return glm::quat(glm::vec3(rotationEuler.z, rotationEuler.x, rotationEuler.y));
+    case EULER_ZYX:
+        return glm::quat(glm::vec3(rotationEuler.z, rotationEuler.y, rotationEuler.x));
+    case QUATERNION:
+        return rotationQuaternion;
+    default:
+        return glm::quat(1.0f, 0.0f, 0.0f, 0.0f);
+    }
+}
+
+SceneObject::SceneObject(Type type, const std::string& name)
+    : m_type(type)
+    , m_parent(nullptr)
+{
+    setName(name);
+
+    if(m_name != getRootName())
+        getObjects()[getRootName()]->addChild(*this);
+}
+
+SceneObject::~SceneObject()
+{
+    if(m_name == getRootName())
+    {
+        logger.warnf("The root object is being deleted. This should not happen.");
+        return;
+    }
+
+    removeAllChildren();
+
+    if(m_parent)
+        m_parent->removeChild(*this);
+
+    getObjects().erase(m_name);
+}
+
+SceneObject::Type SceneObject::getType() const
+{
+    return m_type;
+}
+
+const std::string& SceneObject::getName() const
+{
+    return m_name;
+}
+
+// Sets the name of the object. If the name is already taken, it will be
+// appended with a number.
+void SceneObject::setName(const std::string& name)
+{
+    if(m_name == getRootName())
+        return;
+
+    getObjects().erase(m_name);
+
+    int i = 0;
+    std::string newName = name;
+    char extension[32] = {0};
+
+    while(getObjects().count(newName) > 0)
+    {
+        sprintf(extension, ".%03d", i++);
+        newName = name + extension;
+    }
+
+    m_name = newName;
+
+    getObjects()[newName] = this;
+}
+
+// Removes all chidren and parents them to this object's parent
+void SceneObject::removeAllChildren()
+{
+    if(m_children.empty() || m_name == getRootName())
+        return;
+
+    // Set all children's parent to this object's parent
+    for(auto& child : m_children)
+        child->m_parent = m_parent;
+
+    // Add all children to this object's parent
+    if(m_parent)
+        m_parent->m_children.splice_after(m_parent->m_children.before_begin(), m_children);
+
+    // Clear this object's children
+    m_children.clear();
+}
+
+// Removes a child from this object's children and parents it to this object's
+// parent
+void SceneObject::removeChild(SceneObject& child)
+{
+    if(m_name == getRootName())
+        return;
+
+    // Set child's parent to this object's parent
+    child.m_parent = m_parent;
+
+    // Add child to this object's parent
+    if(m_parent)
+        m_parent->m_children.push_front(&child);
+
+    // Remove child from this object's children
+    m_children.remove(&child);
+}
+
+// Adds a child to this object's children and parents it to this object
+void SceneObject::addChild(SceneObject& child)
+{
+    // Set child's parent to this object
+    child.m_parent = this;
+
+    // Add child to this object's children
+    m_children.push_front(&child);
+}
+
+std::unordered_map<std::string, SceneObject*>& SceneObject::getObjects()
+{
+    static std::unordered_map<std::string, SceneObject*>* objects = nullptr;
+    
+    class Root : public SceneObject
+    {
+    public:
+        Root()
+            : SceneObject(ROOT)
+        {
+            setName(getRootName());
+        }
+
+        ~Root()
+        {
+        }
+
+        void update(float dt) override
+        {
+        }
+
+        void draw() override
+        {
+        }
+    };
+
+    if(!objects)
+    {
+        objects = new std::unordered_map<std::string, SceneObject*>();
+        (*objects)[getRootName()] = new Root();
+    }
+
+    return *objects;
+}
+
+const char* SceneObject::getRootName()
+{
+    static const char* rootName = "Root";
+    return rootName;
+}

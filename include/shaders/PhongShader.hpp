37d8c94ed30e490726f480fe71a9663306b34261
elmfrain <elmfer10@gmail.com>
2023-12-15 02:04:49 -0600

    make phong shader able to specify lights and material

--- include/shaders/PhongShader.hpp
+++ include/shaders/PhongShader.hpp
@@ -2,8 +2,34 @@
 
 #include "Shader.hpp"
 
+#define MAX_LIGHTS 8
+
 namespace em
 {
+    struct PhongMaterial
+    {
+        glm::vec3 ambient = glm::vec3(0.0f);
+        glm::vec3 diffuse = glm::vec3(1.0f);
+        glm::vec3 specular = glm::vec3(1.0f);
+        float shininess = 32.0f;
+
+        void roughnessToShininess(float roughness)
+        {
+            shininess = 1.0f / (roughness * roughness);
+        }
+    };
+
+    struct PhongLight
+    {
+        glm::vec3 position = glm::vec3(0.0f);
+        glm::vec3 emission = glm::vec3(0.0f);
+
+        void setLight(glm::vec3 color, float intensity)
+        {
+            emission = color * intensity;
+        }
+    };
+
     class PhongShader : public Shader
     {
     public:
@@ -12,10 +38,26 @@ namespace em
         bool init() override;
 
         void setCameraPos(const glm::vec3& pos);
+
+        void setMaterial(const PhongMaterial& material);
+        PhongMaterial getMaterial() const;
+
+        PhongLight& getLight(int index);
+        void setLightCount(int count);
     private:
         GLuint m_cameraPosUniformLocation;
+        GLuint m_materialAmbientUniformLocation;
+        GLuint m_materialDiffuseUniformLocation;
+        GLuint m_materialSpecularUniformLocation;
+        GLuint m_materialShininessUniformLocation;
+        GLuint m_lightsPositionUniformLocation[MAX_LIGHTS];
+        GLuint m_lightsEmissionUniformLocation[MAX_LIGHTS];
+        GLuint m_lightCountUniformLocation;
 
         glm::vec3 m_cameraPos;
+        PhongMaterial m_material;
+        PhongLight m_lights[MAX_LIGHTS];
+        int m_lightCount;
 
         static const char* m_vertexShaderSource;
         static const char* m_fragmentShaderSource;
--- src/shaders/PhongShader.cpp
+++ src/shaders/PhongShader.cpp
@@ -37,21 +37,49 @@ const char* PhongShader::m_fragmentShaderSource =
     "uniform vec4 u_color;\n"
     "uniform bool u_vertexColorEnabled;\n"
     "uniform vec3 u_cameraPos;\n"
-    "vec3 lightPos = vec3(0.0, 2.0, 0.0);"
+    "struct Material\n"
+    "{\n"
+    "    vec3 ambient;\n"
+    "    vec3 diffuse;\n"
+    "    vec3 specular;\n"
+    "    float shininess;\n"
+    "};\n"
+    "uniform Material u_material;\n"
+    "struct Light\n"
+    "{\n"
+    "    vec3 position;\n"
+    "    vec3 emission;\n"
+    "};\n"
+    "uniform Light u_lights[8];\n"
+    "uniform int u_lightCount;\n"
+    "vec3 calcLight(Light light, vec3 viewDir)\n"
+    "{\n"
+    "    float distance = length(light.position - fragPos);\n"
+    "    vec3 lightDir = normalize(light.position - fragPos);\n"
+    "    float diff = max(dot(normal, lightDir), 0.0);\n"
+    "    vec3 reflectDir = reflect(-lightDir, normal);\n"
+    "    float spec = pow(max(dot(viewDir, reflectDir), 0.0), u_material.shininess);\n"
+    "    vec3 ambient = light.emission * u_material.ambient;\n"
+    "    vec3 diffuse = light.emission * diff * u_material.diffuse / (distance * distance + 1.0);\n"
+    "    vec3 specular = light.emission * spec * u_material.specular;\n"
+    "    return (ambient + diffuse + specular);\n"
+    "}\n"
     "void main()\n"
     "{\n"
-    "    vec3 lightDir = normalize(lightPos - fragPos);\n"
     "    vec3 viewDir = normalize(u_cameraPos - fragPos);\n"
-    "    vec3 reflectDir = reflect(-lightDir, normal);\n"
-    "    float distance = length(lightPos - fragPos);\n"
-    "    float intensity = 4.0 / (distance * distance);\n"
-    "    float diff = max(dot(normal, lightDir), 0.0);\n"
-    "    float spec = pow(max(dot(viewDir, reflectDir), 0.0), 16);\n"
-    "    outColor = vec4(vec3(diff * intensity + spec), 1.0);\n"
+    "    for(int i = 0; i < u_lightCount; i++)\n"
+    "    {\n"
+    "        outColor += vec4(calcLight(u_lights[i], viewDir), 1.0);\n"
+    "    }\n"
+    "    outColor *= u_color;\n"
+    "    if(u_vertexColorEnabled) outColor *= color;\n"
     "    if(u_enabledTexture >= 0) outColor *= texture(u_textures[u_enabledTexture], uv);\n"
     "}\n";
 
 PhongShader::PhongShader() : Shader()
+    , m_cameraPos(0.0f)
+    , m_material()
+    , m_lightCount(0)
 {
     m_name = "PhongShader";
 }
@@ -63,6 +91,20 @@ bool PhongShader::init()
 
     getDefaultUniformLocations();
     m_cameraPosUniformLocation = glGetUniformLocation(m_program, "u_cameraPos");
+    m_materialAmbientUniformLocation = glGetUniformLocation(m_program, "u_material.ambient");
+    m_materialDiffuseUniformLocation = glGetUniformLocation(m_program, "u_material.diffuse");
+    m_materialSpecularUniformLocation = glGetUniformLocation(m_program, "u_material.specular");
+    m_materialShininessUniformLocation = glGetUniformLocation(m_program, "u_material.shininess");
+    m_lightCountUniformLocation = glGetUniformLocation(m_program, "u_lightCount");
+
+    for(int i = 0; i < MAX_LIGHTS; i++)
+    {
+        char buffer[64];
+        snprintf(buffer, sizeof(buffer), "u_lights[%d].position", i);
+        m_lightsPositionUniformLocation[i] = glGetUniformLocation(m_program, buffer);
+        snprintf(buffer, sizeof(buffer), "u_lights[%d].emission", i);
+        m_lightsEmissionUniformLocation[i] = glGetUniformLocation(m_program, buffer);
+    }
 
     return true;
 }
@@ -72,9 +114,40 @@ void PhongShader::setCameraPos(const glm::vec3& pos)
     m_cameraPos = glm::vec3(pos.x, pos.y, -pos.z);
 }
 
+void PhongShader::setMaterial(const PhongMaterial& material)
+{
+    m_material = material;
+}
+
+PhongMaterial PhongShader::getMaterial() const
+{
+    return m_material;
+}
+
+PhongLight& PhongShader::getLight(int index)
+{
+    return m_lights[index];
+}
+
+void PhongShader::setLightCount(int count)
+{
+    m_lightCount = count;
+}
+
 void PhongShader::updateUniforms()
 {
     Shader::updateUniforms();
 
     glUniform3fv(m_cameraPosUniformLocation, 1, &m_cameraPos[0]);
+    glUniform3fv(m_materialAmbientUniformLocation, 1, &m_material.ambient[0]);
+    glUniform3fv(m_materialDiffuseUniformLocation, 1, &m_material.diffuse[0]);
+    glUniform3fv(m_materialSpecularUniformLocation, 1, &m_material.specular[0]);
+    glUniform1f(m_materialShininessUniformLocation, m_material.shininess);
+    glUniform1i(m_lightCountUniformLocation, m_lightCount);
+
+    for(int i = 0; i < m_lightCount; i++)
+    {
+        glUniform3fv(m_lightsPositionUniformLocation[i], 1, &m_lights[i].position[0]);
+        glUniform3fv(m_lightsEmissionUniformLocation[i], 1, &m_lights[i].emission[0]);
+    }
 }

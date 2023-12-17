7d474ade164f7958bbc01884ad67aea9305276f5
elmfrain <elmfer10@gmail.com>
2023-12-17 01:28:10 -0600

    made framebuffer object

---
+++ include/Framebuffer.hpp
@@ -0,0 +1,41 @@
+#pragma once 
+
+#include <GLInclude.hpp>
+#include <Logger.hpp>
+
+namespace em
+{
+    class Framebuffer
+    {
+    public:
+        Framebuffer(int width, int height);
+        ~Framebuffer();
+
+        void init();
+        void bind();
+        void unbind();
+        void destroy();
+
+        int getWidth() const;
+        int getHeight() const;
+
+        void resize(int width, int height);
+
+        GLuint getTexture() const;
+        GLuint getHandle() const;
+
+        void clearColors();
+        void clearDepth();
+        void clearStencil();
+        void clear();
+    private:
+        int m_width;
+        int m_height;
+
+        GLuint m_framebuffer;
+        GLuint m_colorTexture;
+        GLuint m_depthStencilRenderbuffer;
+
+        static Logger logger;
+    };
+} // namespace em

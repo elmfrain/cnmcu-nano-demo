f2257338a9aa387c0d74c7112c29425b219c3cfa
elmfrain <elmfer10@gmail.com>
2023-12-17 03:08:13 -0600

    set texture parameters for framebuffer

--- src/Framebuffer.cpp
+++ src/Framebuffer.cpp
@@ -25,6 +25,10 @@ void Framebuffer::init(int width, int height)
 
     glGenTextures(1, &m_colorTexture);
     glBindTexture(GL_TEXTURE_2D, m_colorTexture);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
 
     glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB16F, m_width, m_height, 0, GL_RGB, GL_UNSIGNED_BYTE, nullptr);
 

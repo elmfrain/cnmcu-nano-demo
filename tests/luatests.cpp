3e161be6ba04809813f221790e854ee2c9fadbe7
elmfer <elmfer11@outlook.com>
2023-12-20 03:19:26 -0600

    made transform lua interface test

--- tests/luatests.cpp
+++ tests/luatests.cpp
@@ -1,9 +1,104 @@
 #include <gtest/gtest.h>
 
-TEST(LuaScripting, Test1) {
-    EXPECT_EQ(1, 1);
+#include <MeshObject.hpp>
+#include <cstring>
+
+using namespace em;
+
+static const char* luaError;
+static bool luaErrorOccurred = false;
+static char luaErrorBuffer[1024];
+
+static int luaRun(lua_State* L, const char* str)
+{
+    int result = luaL_dostring(L, str);
+    if (result != 0)
+    {
+        luaError = lua_tostring(L, -1);
+        luaErrorOccurred = true;
+        lua_pop(L, 1);
+    }
+    return result;
 }
 
-TEST(LuaScripting, Test1) {
-    EXPECT_EQ(5, 6);
+static int luaAssert(lua_State* L, const char* str)
+{
+    char buffer[1024];
+    snprintf(buffer, 1024, "dummy = %s", str);
+    int result = luaRun(L, buffer);
+    if (result != 0) return result;
+
+    snprintf(buffer, 1024, "assert(%s)", str);
+    return luaRun(L, buffer);
+}
+
+static const char* luaGetError(const char* details)
+{
+    if(!luaErrorOccurred) return "";
+    
+    luaErrorOccurred = false;
+
+    snprintf(luaErrorBuffer, 1024, "\n[Lua] %s:\n\t%s\n", details, luaError);
+    return luaErrorBuffer;
+}
+
+TEST(LuaScripting, Transform)
+{
+    lua_State* L = luaL_newstate();
+    luaL_openlibs(L);
+    Transform::lua_openTransformLib(L);
+
+    Transform t;
+    t.position = glm::vec3(10.0f, 20.0f, 30.0f);
+    t.rotationEuler = glm::vec3(40.0f, 50.0f, 60.0f);
+    t.rotationQuaternion = glm::quat(40.0f, 50.0f, 60.0f, 70.0f);
+    t.scale = glm::vec3(70.0f, 80.0f, 90.0f);
+    t.offset = glm::vec3(100.0f, 110.0f, 120.0f);
+    t.rotationMode = Transform::EULER_YZX;
+    
+    t.lua_this(L);
+    lua_setglobal(L, "t");
+
+    ASSERT_EQ(luaAssert(L, "t:getPosition()[1] == 10.0"), 0) << luaGetError("Transform::getPosition()[1] failed");
+    ASSERT_EQ(luaAssert(L, "t:getPosition()[2] == 20.0"), 0) << luaGetError("Transform::getPosition()[2] failed");
+    ASSERT_EQ(luaAssert(L, "t:getPosition()[3] == 30.0"), 0) << luaGetError("Transform::getPosition()[3] failed");
+
+    ASSERT_EQ(luaAssert(L, "t:getRotationEuler()[1] == 40.0"), 0) << luaGetError("Transform::getRotationEuler()[1] failed");
+    ASSERT_EQ(luaAssert(L, "t:getRotationEuler()[2] == 50.0"), 0) << luaGetError("Transform::getRotationEuler()[2] failed");
+    ASSERT_EQ(luaAssert(L, "t:getRotationEuler()[3] == 60.0"), 0) << luaGetError("Transform::getRotationEuler()[3] failed");
+
+    ASSERT_EQ(luaAssert(L, "t:getRotationQuaternion()[1] == 40.0"), 0) << luaGetError("Transform::getRotationQuaternion()[1] failed");
+    ASSERT_EQ(luaAssert(L, "t:getRotationQuaternion()[2] == 50.0"), 0) << luaGetError("Transform::getRotationQuaternion()[2] failed");
+    ASSERT_EQ(luaAssert(L, "t:getRotationQuaternion()[3] == 60.0"), 0) << luaGetError("Transform::getRotationQuaternion()[3] failed");
+    ASSERT_EQ(luaAssert(L, "t:getRotationQuaternion()[4] == 70.0"), 0) << luaGetError("Transform::getRotationQuaternion()[4] failed");
+
+    ASSERT_EQ(luaAssert(L, "t:getScale()[1] == 70.0"), 0) << luaGetError("Transform::getScale()[1] failed");
+    ASSERT_EQ(luaAssert(L, "t:getScale()[2] == 80.0"), 0) << luaGetError("Transform::getScale()[2] failed"); 
+    ASSERT_EQ(luaAssert(L, "t:getScale()[3] == 90.0"), 0) << luaGetError("Transform::getScale()[3] failed");
+
+    ASSERT_EQ(luaAssert(L, "t:getOffset()[1] == 100.0"), 0) << luaGetError("Transform::getOffset()[1] failed");
+    ASSERT_EQ(luaAssert(L, "t:getOffset()[2] == 110.0"), 0) << luaGetError("Transform::getOffset()[2] failed");
+    ASSERT_EQ(luaAssert(L, "t:getOffset()[3] == 120.0"), 0) << luaGetError("Transform::getOffset()[3] failed");
+
+    ASSERT_EQ(luaAssert(L, "t:getRotationMode() == \"EULER_YZX\""), 0) << luaGetError("Transform::getRotationMode() failed");
+
+    ASSERT_EQ(luaRun(L, "t:setPosition({1.0, 2.0, 3.0})"), 0) << luaGetError("Transform::setPosition() failed");
+    ASSERT_EQ(t.position, glm::vec3(1.0f, 2.0f, 3.0f)) << "Transform::setPosition() failed";
+
+    ASSERT_EQ(luaRun(L, "t:setRotationEuler({4.0, 5.0, 6.0})"), 0) << luaGetError("Transform::setRotationEuler() failed");
+    ASSERT_EQ(t.rotationEuler, glm::vec3(4.0f, 5.0f, 6.0f)) << "Transform::setRotationEuler() failed";
+
+    ASSERT_EQ(luaRun(L, "t:setRotationQuaternion({7.0, 8.0, 9.0, 10.0})"), 0) << luaGetError("Transform::setRotationQuaternion() failed");
+    ASSERT_EQ(t.rotationQuaternion, glm::quat(7.0f, 8.0f, 9.0f, 10.0f)) << "Transform::setRotationQuaternion() failed";
+
+    ASSERT_EQ(luaRun(L, "t:setScale({11.0, 12.0, 13.0})"), 0) << luaGetError("Transform::setScale() failed");
+    ASSERT_EQ(t.scale, glm::vec3(11.0f, 12.0f, 13.0f)) << "Transform::setScale() failed";
+
+    ASSERT_EQ(luaRun(L, "t:setOffset({14.0, 15.0, 16.0})"), 0) << luaGetError("Transform::setOffset() failed");
+    ASSERT_EQ(t.offset, glm::vec3(14.0f, 15.0f, 16.0f)) << "Transform::setOffset() failed";
+
+    ASSERT_EQ(luaRun(L, "t:setRotationMode(\"EULER_XYZ\")"), 0) << luaGetError("Transform::setRotationMode() failed");
+    ASSERT_EQ(t.rotationMode, Transform::EULER_XYZ) << "Transform::setRotationMode() failed";
+
+    lua_close(L);
 }

c8d1559f86a0831a41cd8b07bd45039ed8356f05
elmfrain <elmfer10@gmail.com>
2023-12-19 22:12:15 -0600

    added lua interface for Camera

--- include/Camera.hpp
+++ include/Camera.hpp
@@ -9,14 +9,16 @@ namespace em
     public:
         enum ProjectionMode
         {
-            PERSPECTIVE,
+            PERSPECTIVE = 0,
             ORTHOGRAPHIC
         };
+        static const char* const ProjectionModeNames[];
         enum FOVMode
         {
-            VERTICAL,
+            VERTICAL = 0,
             HORIZONTAL
         };
+        static const char* const FOVModeNames[];
 
         Camera(const std::string& name = "Camera");
         virtual ~Camera();
@@ -37,5 +39,24 @@ namespace em
         const glm::mat4 getProjectionMatrix() const;
         const glm::mat4 getViewMatrix() const;
         const glm::mat4 getViewProjectionMatrix() const;
+
+        int lua_this(lua_State* L);
+        static int lua_openCameraLib(lua_State* L);
+    private:
+        static int lua_getProjectionMode(lua_State* L);
+        static int lua_getFOVMode(lua_State* L);
+        static int lua_getOrthographicSize(lua_State* L);
+        static int lua_getFieldOfView(lua_State* L);
+        static int lua_getNearPlane(lua_State* L);
+        static int lua_getFarPlane(lua_State* L);
+        static int lua_getPerspectiveShift(lua_State* L);
+
+        static int lua_setProjectionMode(lua_State* L);
+        static int lua_setFOVMode(lua_State* L);
+        static int lua_setOrthographicSize(lua_State* L);
+        static int lua_setFieldOfView(lua_State* L);
+        static int lua_setNearPlane(lua_State* L);
+        static int lua_setFarPlane(lua_State* L);
+        static int lua_setPerspectiveShift(lua_State* L);
     };
 }
--- src/Camera.cpp
+++ src/Camera.cpp
@@ -3,6 +3,9 @@
 
 using namespace em;
 
+const char* const Camera::ProjectionModeNames[] = { "Perspective", "Orthographic" };
+const char* const Camera::FOVModeNames[] = { "Vertical", "Horizontal" };
+
 Camera::Camera(const std::string& name)
     : SceneObject(CAMERA, name)
     , projectionMode(PERSPECTIVE)
@@ -61,4 +64,172 @@ const glm::mat4 Camera::getViewMatrix() const
 const glm::mat4 Camera::getViewProjectionMatrix() const
 {
     return getProjectionMatrix() * getViewMatrix();
+}
+
+#define luaGetCamera() \
+    Camera* camera; \
+    luaPushValueFromKey("ptr", 1); \
+    luaGetPointer(camera, Camera, -1);
+
+int Camera::lua_this(lua_State* L)
+{
+    lua_newtable(L);
+    lua_pushstring(L, "ptr");
+    lua_pushlightuserdata(L, this);
+    lua_settable(L, -3);
+    lua_pushstring(L, "transform");
+    getTransform().lua_this(L);
+    lua_settable(L, -3);
+    
+    luaL_newmetatable(L, "Camera");
+    lua_setmetatable(L, -2);
+
+    return 1;
+}
+
+int Camera::lua_openCameraLib(lua_State* L)
+{
+    static const luaL_Reg cameraLib[] = {
+        { "getProjectionMode", lua_getProjectionMode },
+        { "getFOVMode", lua_getFOVMode },
+        { "getOrthographicSize", lua_getOrthographicSize },
+        { "getFieldOfView", lua_getFieldOfView },
+        { "getNearPlane", lua_getNearPlane },
+        { "getFarPlane", lua_getFarPlane },
+        { "getPerspectiveShift", lua_getPerspectiveShift },
+        { "setProjectionMode", lua_setProjectionMode },
+        { "setFOVMode", lua_setFOVMode },
+        { "setOrthographicSize", lua_setOrthographicSize },
+        { "setFieldOfView", lua_setFieldOfView },
+        { "setNearPlane", lua_setNearPlane },
+        { "setFarPlane", lua_setFarPlane },
+        { "setPerspectiveShift", lua_setPerspectiveShift },
+        { nullptr, nullptr }
+    };
+
+    luaL_newmetatable(L, "Camera");
+    luaL_setfuncs(L, cameraLib, 0);
+
+    lua_pushstring(L, "__index");
+    lua_pushvalue(L, -2);
+    lua_settable(L, -3);
+
+    luaL_newmetatable(L, "SceneObject");
+    lua_setmetatable(L, -2);
+
+    lua_setglobal(L, "Camera");
+
+    return 0;
+}
+
+int Camera::lua_getProjectionMode(lua_State* L)
+{
+    luaGetCamera();
+    lua_pushstring(L, ProjectionModeNames[camera->projectionMode]);
+
+    return 1;
+}
+
+int Camera::lua_getFOVMode(lua_State* L)
+{
+    luaGetCamera();
+    lua_pushstring(L, FOVModeNames[camera->fovMode]);
+
+    return 1;
+}
+
+int Camera::lua_getOrthographicSize(lua_State* L)
+{
+    luaGetCamera();
+    lua_pushnumber(L, camera->orthographicSize);
+
+    return 1;
+}
+
+int Camera::lua_getFieldOfView(lua_State* L)
+{
+    luaGetCamera();
+    lua_pushnumber(L, camera->fieldOfView);
+
+    return 1;
+}
+
+int Camera::lua_getNearPlane(lua_State* L)
+{
+    luaGetCamera();
+    lua_pushnumber(L, camera->nearPlane);
+
+    return 1;
+}
+
+int Camera::lua_getFarPlane(lua_State* L)
+{
+    luaGetCamera();
+    lua_pushnumber(L, camera->farPlane);
+
+    return 1;
+}
+
+int Camera::lua_getPerspectiveShift(lua_State* L)
+{
+    luaGetCamera();
+    luaPushVec2(camera->perspectiveShift);
+
+    return 2;
+}
+
+int Camera::lua_setProjectionMode(lua_State* L)
+{
+    luaGetCamera();
+    camera->projectionMode = static_cast<ProjectionMode>(luaL_checkoption(L, 2, nullptr, ProjectionModeNames));
+
+    return 0;
+}
+
+int Camera::lua_setFOVMode(lua_State* L)
+{
+    luaGetCamera();
+    camera->fovMode = static_cast<FOVMode>(luaL_checkoption(L, 2, nullptr, FOVModeNames));
+
+    return 0;
+}
+
+int Camera::lua_setOrthographicSize(lua_State* L)
+{
+    luaGetCamera();
+    luaGet(camera->orthographicSize, float, number, 2);
+
+    return 0;
+}
+
+int Camera::lua_setFieldOfView(lua_State* L)
+{
+    luaGetCamera();
+    luaGet(camera->fieldOfView, float, number, 2);
+
+    return 0;
+}
+
+int Camera::lua_setNearPlane(lua_State* L)
+{
+    luaGetCamera();
+    luaGet(camera->nearPlane, float, number, 2);
+
+    return 0;
+}
+
+int Camera::lua_setFarPlane(lua_State* L)
+{
+    luaGetCamera();
+    luaGet(camera->farPlane, float, number, 2);
+
+    return 0;
+}
+
+int Camera::lua_setPerspectiveShift(lua_State* L)
+{
+    luaGetCamera();
+    luaGetVec2(camera->perspectiveShift, 2);
+
+    return 0;
 }
